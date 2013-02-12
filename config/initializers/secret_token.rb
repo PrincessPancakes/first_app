31997504e9e90252d4821ae1b930258c04897ab2
Anand Dhillon <anand180@gmail.com>
2013-02-12 12:43:44 -0500

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Stylekick::Application.config.secret_token = 'fd1693c4d1bc393c8d7baf7f9ff0956c260e1be007904d09c630bab9f27d2691fa9579508b02b9603cfcdf2572ecaeae67bf7421841a566c019d3c1a6ae35783'
