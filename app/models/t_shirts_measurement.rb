97236ee226567008796d8c9d579adc680ca5f3d3
Anand Dhillon <anand180@gmail.com>
2013-02-27 15:38:57 -0500

    fixed validations

--- app/models/t_shirts_measurement.rb
+++ app/models/t_shirts_measurement.rb
@@ -5,14 +5,14 @@ class TShirtsMeasurement < Measurement
 
   validates :waist, :opening, :hps_length, :cb_length, :chest, :sleeve_width,
             :shoulder, :arm_hole, presence: true
-  validates :waist, numericality: { greater_than_or_equal_to: 12, less_than_or_equal_to: 25 }
-  validates :opening, numericality: { greater_than_or_equal_to: 12, less_than_or_equal_to: 25 }
-  validates :chest, numericality: { greater_than_or_equal_to: 14, less_than_or_equal_to: 25 }
-  #validates :hps_length, numericality: { greater_than_or_equal_to: 25, less_than_or_equal_to: 35 }
-  #validates :cb_length, numericality: { greater_than_or_equal_to: 25, less_than_or_equal_to: 35 }
-  #validates :shoulder, numericality: { greater_than_or_equal_to: 15, less_than_or_equal_to: 25 }
-  validates :arm_hole, numericality: { greater_than_or_equal_to: 6, less_than_or_equal_to: 12 }
-  validates :sleeve_width, numericality: { greater_than_or_equal_to: 5, less_than_or_equal_to: 13 }
+  #validates :waist, numericality: { greater_than_or_equal_to: 12, less_than_or_equal_to: 25 }
+  #validates :opening, numericality: { greater_than_or_equal_to: 12, less_than_or_equal_to: 25 }
+  #validates :chest, numericality: { greater_than_or_equal_to: 14, less_than_or_equal_to: 25 }
+  ##validates :hps_length, numericality: { greater_than_or_equal_to: 25, less_than_or_equal_to: 35 }
+  ##validates :cb_length, numericality: { greater_than_or_equal_to: 25, less_than_or_equal_to: 35 }
+  ##validates :shoulder, numericality: { greater_than_or_equal_to: 15, less_than_or_equal_to: 25 }
+  #validates :arm_hole, numericality: { greater_than_or_equal_to: 6, less_than_or_equal_to: 12 }
+  #validates :sleeve_width, numericality: { greater_than_or_equal_to: 5, less_than_or_equal_to: 13 }
 
 
 
